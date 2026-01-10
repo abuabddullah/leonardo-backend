a701c4b05a4c06c170760d8a1933866ebe44850a
asifaowadud <asifaowadud@gmail.com>
2026-01-10 16:56:08 +0600

    mend

--- src/app/builder/QueryBuilder.ts
+++ src/app/builder/QueryBuilder.ts
@@ -1,6 +1,4 @@
 import { FilterQuery, Query } from 'mongoose';
-import AppError from '../../errors/AppError';
-import { StatusCodes } from 'http-status-codes';
 
 class QueryBuilder<T> {
      public modelQuery: Query<T[], T>;
@@ -11,14 +9,17 @@ class QueryBuilder<T> {
           this.query = query;
      }
 
+     //searching
      search(searchableFields: string[]) {
-          const searchTerm = this.query?.searchTerm as string;
-          if (searchTerm) {
+          if (this?.query?.searchTerm) {
                this.modelQuery = this.modelQuery.find({
                     $or: searchableFields.map(
                          (field) =>
                               ({
-                                   [field]: { $regex: searchTerm, $options: 'i' },
+                                   [field]: {
+                                        $regex: this.query.searchTerm,
+                                        $options: 'i',
+                                   },
                               }) as FilterQuery<T>,
                     ),
                });
@@ -26,62 +27,73 @@ class QueryBuilder<T> {
           return this;
      }
 
+     //filtering
      filter() {
-          const excludeFields = ['searchTerm', 'sort', 'limit', 'page', 'fields', 'maxPrice', 'minPrice'];
           const queryObj = { ...this.query };
+          const excludeFields = ['searchTerm', 'sort', 'page', 'limit', 'fields'];
           excludeFields.forEach((el) => delete queryObj[el]);
 
-          this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>);
+          // this.modelQuery = this.modelQuery.find(queryObj as FilterQuery<T>); // this line of code is for strict value filtering but considering rangeable filter like filtering for price range or rating range this line will fail soo need ot comment out and work as bellow
+
+          // Filter For Price and Rating
+          let queryStr = JSON.stringify(queryObj);
+          queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key) => `$${key}`);
+          this.modelQuery = this.modelQuery.find(JSON.parse(queryStr));
+
           return this;
      }
 
+     //sorting
      sort() {
-          const sort = (this.query?.sort as string)?.split(',')?.join(' ') || '-createdAt';
-          this.modelQuery = this.modelQuery.sort(sort as string);
+          let sort = (this?.query?.sort as string) || '-createdAt';
+          this.modelQuery = this.modelQuery.sort(sort);
+
           return this;
      }
 
-     paginate(defaultLimit = 10) {
-          const page = Number(this.query?.page) || 1;
-          const limit = Number(this.query?.limit) || defaultLimit;
-          const skip = (page - 1) * limit;
+     //pagination
+     paginate() {
+          let limit = Number(this?.query?.limit) || 10;
+          let page = Number(this?.query?.page) || 1;
+          let skip = (page - 1) * limit;
 
-          this.modelQuery = this.modelQuery.skip(skip).limit(limit).sort();
+          this.modelQuery = this.modelQuery.skip(skip).limit(limit);
+
           return this;
      }
 
+     //fields filtering
      fields() {
-          const fields = (this.query?.fields as string)?.split(',')?.join(' ') || '-__v';
+          let fields = (this?.query?.fields as string)?.split(',').join(' ') || '-__v';
           this.modelQuery = this.modelQuery.select(fields);
-          return this;
-     }
-     priceRange() {
-          const priceFilter: Record<string, unknown> = {};
-          const minPrice = this.query?.minPrice as number;
-          const maxPrice = this.query?.maxPrice as number;
-          if (minPrice !== undefined) priceFilter.$gte = minPrice;
-          if (maxPrice !== undefined) priceFilter.$lte = maxPrice;
-
-          if (minPrice !== undefined || maxPrice !== undefined) {
-               this.modelQuery = this.modelQuery.find({
-                    price: priceFilter,
-               } as FilterQuery<T>);
-          }
 
           return this;
      }
+
+     //populating
+     populate(populateFields: string[], selectFields: Record<string, unknown>) {
+          this.modelQuery = this.modelQuery.populate(
+               populateFields.map((field) => ({
+                    path: field,
+                    select: selectFields[field],
+               })),
+          );
+          return this;
+     }
+
+     //pagination information
      async countTotal() {
-          try {
-               const totalQueries = this.modelQuery.getFilter();
-               const total = await this.modelQuery.model.countDocuments(totalQueries);
-               const page = Number(this.query?.page) || 1;
-               const limit = Number(this.query?.limit) || 10;
-               const totalPage = Math.ceil(total / limit);
+          const total = await this.modelQuery.model.countDocuments(this.modelQuery.getFilter());
+          const limit = Number(this?.query?.limit) || 10;
+          const page = Number(this?.query?.page) || 1;
+          const totalPage = Math.ceil(total / limit);
 
-               return { page, limit, total, totalPage };
-          } catch (error) {
-               throw new AppError(StatusCodes.SERVICE_UNAVAILABLE, error as string);
-          }
+          return {
+               total,
+               limit,
+               page,
+               totalPage,
+          };
      }
 }
 
